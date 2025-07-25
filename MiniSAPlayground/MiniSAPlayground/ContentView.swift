0715d3f12d44f89339b5e9d4886f3beb05425ad5
Leo Wirasanto Laia <leowirasanto@gmail.com>
2025-07-26 04:03:35 +0800

    dropping the first bom

---
+++ MiniSAPlayground/MiniSAPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiniSAPlayground
+//
+//  Created by Leo Wirasanto Laia on 26/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiniSAPlayground/MiniSAPlayground/MiniSAPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniSAPlaygroundApp.swift
+//  MiniSAPlayground
+//
+//  Created by Leo Wirasanto Laia on 26/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MiniSAPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
